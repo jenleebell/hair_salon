78353c564de37ec34a3ad5cb9b02c5828b09600b
Jennifer Bell <jenleebell@gmail.com>
2015-08-21 10:30:42 -0700

    Add save method to stylist class

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -7,15 +7,24 @@ class Stylist
     @id = attr.fetch(:id)
   end
 
-define_singleton_method(:all) do
+  define_singleton_method(:all) do
     returned_stylists = DB.exec("SELECT * FROM stylists;")
     stylists = []
     returned_stylists.each() do |stylist|
       name = stylist.fetch("name")
-      gender = stylist.fetch("phone")
+      phone = stylist.fetch("phone")
       id = stylist.fetch("id").to_i()
       stylists.push(Stylist.new({:name => name, :phone => phone, :id => id}))
     end
     stylists
   end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO stylists (name, phone, id) VALUES ('#{@name}', '#{@phone}', '#{@id}');")
+  end
+
+  define_method(:==) do |another_stylist|
+  self.name().==(another_stylist.name())
+end
+
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -15,4 +15,20 @@ describe(Stylist) do
     end
   end
 
+  describe('#save') do
+    it('adds an stylist to the array of saved stylists') do
+      test_stylist = Stylist.new({:name => "Han Solo", :phone => "5038675309", :id => 1})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same stylist if it has the same name") do
+      test_stylist1 = Stylist.new({:name => "Han Solo", :phone => "5038675309", :id => nil})
+      test_stylist2 = Stylist.new({:name => "Han Solo", :phone => "5038675309", :id => nil})
+      expect(test_stylist1).to(eq(test_stylist2))
+    end
+  end
+
 end
