890b59460a0b0aed5faa8544fc2a8a3d003c778a
Jennifer Bell <jenleebell@gmail.com>
2015-08-21 14:15:18 -0700

    Add layouts for all pages

---
+++ app.rb
@@ -0,0 +1,63 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/client")
+require("./lib/stylist")
+require("pg")
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+get('/') do
+  erb(:index)
+end
+
+get("/stylists/new") do
+  erb(:stylist_form)
+end
+
+post('/stylists') do
+  name = params.fetch('name')
+  phone = params.fetch('phone')
+  stylist = Stylist.new({:name => name, :phone => phone, :id => nil})
+  stylist.save()
+  erb(:success)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  erb(:stylist)
+end
+
+post("/clients") do
+  name = params.fetch("name")
+  phone = params.fetch('phone')
+  stylist_id = params.fetch("stylist_id").to_i()
+  @stylist = Stylist.find(client_id)
+  @client = Client.new({:title => title, :phone => phone, :client_id => client_id})
+  @client.save()
+  erb(:success)
+end
+
+get("/stylists/:id/edit") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  erb(:stylist_edit)
+end
+
+patch("/stylists/:id") do
+  name = params.fetch("name")
+  phone = params.fetch("phone")
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.update({:name => name, :phone => phone})
+  erb(:stylist)
+end
+
+delete("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.delete()
+  @stylists = Stylist.all()
+end
